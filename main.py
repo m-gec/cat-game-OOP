6db4fb5c56bc742ab61d1f012b3066771bb1b31f
m-gec <mustafa.gec@laetottenham.ac.uk>
2024-11-11 12:06:02 +0000

    blank wojghiudwugerrgw

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from cat_container import cat
+import sys
 
-cat_one = cat("cat one", "black")
-cat_two = cat("cat two", "white")
+#create a cat
+cat_name = input("\nname your cat\n")
+cat_color = input("\nenter your cat's color\n")
+cat_one = cat(cat_name, cat_color)
 
+#game loop
+while True:
+    response = input("\nchoose an option from the list:\n1. train\n2. feed\n3. put to sleep\n4. exit\n")
+    if not response.isdigit():
+        print("\ninvalid input, please retry\n")
+        continue
 
-cat_one.feed()
-cat_one.spill()
+    match response:
+        case "1":
+            print(f"{cat_one.name} was trained\n")
+            cat_one.train()
+        case "2":
+            print(f"{cat_one.name} was fed\n")
+            cat_one.feed()
+        case "3":
+            print(f"{cat_one.name} was put to sleep\n")
+            cat_one.sleep()
+        case "4":
+            print(f"ended the program\n")
+            sys.exit()
+        case _:
+            print("invalid input, please retry")
+            continue
+    
+    print(f"{cat_one.name}'s stats:\n")
+    cat_one.spill()
